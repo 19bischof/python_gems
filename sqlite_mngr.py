b55b0cc02dd5f2083a3363e1891f7f7d08409e9c
Moritz <home@gmail.com>
2022-04-01 20:08:29 +0200

    fixed and improved faulty functions and added some utility

--- sqlite_mngr.py
+++ sqlite_mngr.py
@@ -6,17 +6,24 @@ import pathlib
 
 class sqlite3_functions:
 
+    no_out = False
+
     def __init__(self, file_name="my_db.db"):
-        self.file_path = pathlib.Path(__file__).parent.as_posix() + "/" + file_name
+        self.file_path = pathlib.Path(
+            __file__).parent.as_posix() + "/" + file_name
 
     @staticmethod
     def mng_conn(bosom):
         """connection context manager"""
-        def ample(self,*args):
+
+        def ample(self, *args):
             with sqlite3.Connection(self.file_path) as conn:
                 self.cursor = conn.cursor()
-                bosom(self,*args)
-                print(prettytable.from_db_cursor(self.cursor))
+                bosom(self, *args)
+                if self.no_out:
+                    return
+                if table_s := prettytable.from_db_cursor(self.cursor):
+                    print(table_s)
         return ample
 
     def scrub(self, s):
@@ -30,23 +37,22 @@ class sqlite3_functions:
         return new
 
     @mng_conn
-    def create_table(self,dirty_table_name:str,columns:Iterable[str]):
+    def create_table(self, dirty_table_name: str, columns: Iterable[str]):
         """creates a table"""
         table_name = self.scrub(dirty_table_name)
         columns = list(columns)
-        for index,c in enumerate(columns):
-            columns[index] = self.scrub(c)
-        params = ("?," * len(columns))[:-1]
-        query = f"CREATE TABLE IF NOT EXISTS {table_name}("+ params + ");"
-        print(query)
-        print(columns)
-        self.cursor.execute(query,columns)
+        end_str = "("
+        for c in columns:
+            end_str += self.scrub(c) + ","
+        end_str = end_str[:-1] + ");"
+        query = f"CREATE TABLE IF NOT EXISTS {table_name} " + end_str
+        self.cursor.execute(query)
 
     @mng_conn
     def drop(self, dirty_table_name: str):
         """drops a table"""
         table_name = self.scrub(dirty_table_name)
-        query = f"""DROP TABLE {table_name};"""
+        query = f"""DROP TABLE IF EXISTS {table_name};"""
         self.cursor.execute(query)
 
     @mng_conn
@@ -64,18 +70,18 @@ class sqlite3_functions:
         self.cursor.execute(query)
 
     @mng_conn
-    def select_some(self, dirty_table_name: str,to_select: Iterable[str]):
+    def select_some(self, dirty_table_name: str, to_select: Iterable[str]):
         """selects some columns from a table"""
         table_name = self.scrub(dirty_table_name)
-        params = ("?," * len(to_select))[:-1]
-        query = "SELECT "  + params + f" FROM {table_name};"
-        self.cursor.execute(query,to_select)
+        cols = ",".join(to_select)
+        query = "SELECT " + cols + f" FROM {table_name};"
+        self.cursor.execute(query)
 
     @mng_conn
     def insert(self, dirty_table_name: str, values: Iterable[Any]):
         """inserts a row with values in a table"""
         table_name = self.scrub(dirty_table_name)
-        params =  ("?," * len(values))[:-1]
+        params = ("?," * len(values))[:-1]
         query = f"INSERT INTO {table_name} VALUES (" + params + ");"
         self.cursor.execute(query, values)
 
@@ -91,26 +97,39 @@ class sqlite3_functions:
     @mng_conn
     def print_tables(self):
         """prints the name of all tables"""
-        query = """SELECT name FROM sqlite_schema
+        query = """SELECT name FROM {}
         WHERE type ='table' 
         AND name NOT LIKE 'sqlite_%';"""
-        self.cursor.execute(query)
-    
+        try:
+            self.cursor.execute(query.format("sqlite_schema"))
+        except sqlite3.OperationalError:
+            self.cursor.execute(query.format("sqlite_master"))
+
     @mng_conn
-    def custom_query(self,query:str,params: Iterable[Any]):
-        self.cursor(query,params)
+    def print_sqlite_version(self):
+        """prints the sqlite version"""
+        query = """SELECT sqlite_version();"""
+        self.cursor.execute(query)
+
+    @mng_conn
+    def custom_query(self, query: str, params: Iterable[Any]):
+        self.cursor(query, params)
+
 
 if __name__ == "__main__":
-    #example usage
+    # example usage
     db = sqlite3_functions()
-    # db.create_table("table1",("test text",))
-    # db.drop("table1")
-    db.create_table("table2",("name text","age int"))
+    db.no_out = False
+    db.create_table("table2",("test text",))
+    db.drop("table2")
+    db.drop("table1")
+    db.create_table("table1",("name text","age int"))
     db.print_tables()
+    db.print_sqlite_version()
     db.describe("table1")
     db.select_all("table1")
-    db.insert("table1",("herald",))
-    db.insert("table1",("hubert",))
+    db.insert("table1",("herald",132))
+    db.insert("table1",("hubert",120))
     db.select_some("table1",("name",))
     db.update("table1",'name = "hammer"',"name = 'hubert'")
     db.select_all("table1")
