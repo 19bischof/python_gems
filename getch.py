927abe512b19081a3da01cd140ca3a8a2d9df00f
lv-15 <lv-15@home.com>
2022-12-21 22:11:27 +0100

    removed unnecessary flush

--- getch.py
+++ getch.py
@@ -1,22 +1,18 @@
 # original: https://code.activestate.com/recipes/134892/
 class Getch:
     """Gets a single character from terminal.  Does not echo to the
-screen. Flushes while creating instance. Call the instance to get character.
+screen. Call the instance to get character.
 Example at bottom"""
-    def __init__(self,flush=True):
+    def __init__(self):
         try:
-            self.impl = _GetchWindows(flush)
+            self.impl = _GetchWindows()
         except ImportError:
-            self.impl = _GetchUnix(flush)
+            self.impl = _GetchUnix()
             
     def __call__(self): return self.impl()
 
 
 class _GetchUnix:
-    def __init__(self,flush):
-        import tty, sys,termios
-        termios.tcflush(sys.stdin, termios.TCIOFLUSH) #to flush previous
-
     def __call__(self):
         import sys, tty, termios
         fd = sys.stdin.fileno()
@@ -30,7 +26,7 @@ class _GetchUnix:
 
 
 class _GetchWindows:
-    def __init__(self,flush):
+    def __init__(self):
         import msvcrt
         while msvcrt.kbhit():
             msvcrt.getch()
@@ -40,7 +36,7 @@ class _GetchWindows:
         return msvcrt.getch()
 
 if __name__ == "__main__":
-    _getch = Getch() #init + flush
+    _getch = Getch() #init 
     while(inp:=_getch()): #get character
         if inp == b"\x03": #Ctrl+c
             quit()
